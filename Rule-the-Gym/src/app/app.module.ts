18cdcb3a46ab43ae15405321de823e30f05ae116
Johannes <johannes.schmaelzle@web.de>
2022-10-20 11:35:19 +0200

    Added material Final

--- Rule-the-Gym/src/app/app.module.ts
+++ Rule-the-Gym/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MaterialModule} from "./modules/material.module"
+import {MaterialModule} from "./modules/material.module";
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,    
+    AppComponent,    
   ],
   imports: [
     BrowserModule,
