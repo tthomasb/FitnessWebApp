df16d0866e640a24368e5f58e05d755901d7aec3
Johannes <johannes.schmaelzle@web.de>
2022-10-20 21:49:20 +0200

    Added Treenode for Workouts to Training (wrongBrach)

--- Rule-the-Gym/src/app/components/training/training.component.ts
+++ Rule-the-Gym/src/app/components/training/training.component.ts
@@ -1,14 +1,44 @@
+import { NestedTreeControl } from '@angular/cdk/tree';
 import { Component, OnInit } from '@angular/core';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 
+interface FoodNode {
+  name: string;
+  pause:string
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Workouts',
+    pause:'2min',
+    children: [
+      {
+        name: 'Workout 1',
+        pause:'2min',
+        children: [{name: 'Exercise 1', pause:'2min'}, {name: 'Exercise 2',pause:'2min'}, {name: 'Exercise 3', pause:'2min'}],
+      },
+      {
+        name: 'Workout 2',
+        pause:'2min',
+        children: [{name: 'Exercise 1',pause:'2min',}, {name: 'Exercise 2',pause:'2min',}, {name: 'Exercise 3',pause:'2min',}],
+      },
+    ],
+  },
+];
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.scss']
 })
 export class TrainingComponent implements OnInit {
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
 
-  constructor() { }
-
+  constructor() { 
+    this.dataSource.data = TREE_DATA;
+  }
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
   ngOnInit(): void {
   }
 
