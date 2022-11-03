1409dcacd93eef4069d969b9935122897fbfc27b
Johannes <johannes.schmaelzle@web.de>
2022-11-03 11:21:56 +0100

    added basic DataModel Objects

--- Rule-the-Gym/src/app/models/workout-data.model.ts
+++ Rule-the-Gym/src/app/models/workout-data.model.ts
@@ -1,4 +1,28 @@
+import { ExerciseModel } from "./exercise-model.model";
+
 export class WorkoutData {
 
-    
+    name?: string;
+    exercises?: ExerciseModel[];
+
+}
+
+export namespace WorkoutData{
+    export function parseWorkoutData(obj:any):WorkoutData{
+        const data=new WorkoutData();
+        let ctr=0;Node
+        for(const key of obj.keys()){
+            if(!obj[key]){
+                throw new Error('Key is empty!')
+            }else{
+                if(obj[key] instanceof ExerciseModel){
+                        const exercises: any[]=[];
+                        exercises[ctr]=ExerciseModel.parseExerciseModel(obj[key]);
+                        ctr++;
+                }
+            }
+            
+        }
+        return new WorkoutData;
+    }
 }
