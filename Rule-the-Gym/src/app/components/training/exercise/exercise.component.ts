109aaade46c5a2759b6b0e1e9b488ffea5452b99
Johannes <johannes.schmaelzle@web.de>
2022-11-10 12:08:23 +0100

    fix

--- Rule-the-Gym/src/app/components/training/exercise/exercise.component.ts
+++ Rule-the-Gym/src/app/components/training/exercise/exercise.component.ts
@@ -2,7 +2,7 @@ import { group } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder} from '@angular/forms';
 import { ExerciseModel } from 'src/app/models/exercise-model.model';
-import { muscleGroup } from 'src/app/models/muscleGroup.model';
+
 
 @Component({
   selector: 'app-exercise',
@@ -11,7 +11,7 @@ import { muscleGroup } from 'src/app/models/muscleGroup.model';
 })
 export class ExerciseComponent implements OnInit {
  exercises:ExerciseModel[]
- groups:muscleGroup[];
+ 
 
   Musclegroup = this._formBuilder.group({
     chest: false,
@@ -22,7 +22,7 @@ export class ExerciseComponent implements OnInit {
 
   constructor(private _formBuilder: FormBuilder) { 
     this.exercises=[new ExerciseModel("Curls","biceps curls",5,4,60,"biceps"),new ExerciseModel("Curls","biceps curls",5,4,60,"biceps")];
-    this.groups=[new muscleGroup("biceps",this.exercises)];
+    
     
   }
 
