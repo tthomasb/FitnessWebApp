1440ee67cb614e298803f685110ca384ff79978b
NickGuenter <98540299+NickGuenter@users.noreply.github.com>
2023-04-06 18:00:58 +0200

    Added first design Idea

--- Rule-the-Gym/src/app/components/bmi/bmi.component.ts
+++ Rule-the-Gym/src/app/components/bmi/bmi.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BmiComponent implements OnInit {
 
+  bmi!: number;
+  weight!: string;
+  height!: string;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Method to calculate BMI based on user input
+  getBmi(): void {
+    const parsedWeight = parseInt(this.weight);
+    const parsedHeight = parseInt(this.height);
+    if (parsedHeight !== 0 && !isNaN(parsedWeight) && !isNaN(parsedHeight)) {
+      this.bmi = parsedWeight / ((parsedHeight * parsedHeight) / 10000);
+      alert(`Your BMI is ${this.bmi.toFixed(2)}`);
+    } else {
+      alert("Fill in valid Data");
+    }
   }
 
 }
