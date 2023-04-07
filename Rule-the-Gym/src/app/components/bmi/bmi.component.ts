a843150ed3005e41f6c944068ee3f15a4d043ef4
NickGuenter <98540299+NickGuenter@users.noreply.github.com>
2023-04-07 20:40:27 +0200

    Update surface

--- Rule-the-Gym/src/app/components/bmi/bmi.component.ts
+++ Rule-the-Gym/src/app/components/bmi/bmi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { createLoopVariable } from 'typescript';
 
 @Component({
   selector: 'app-bmi',
@@ -8,24 +9,58 @@ import { Component, OnInit } from '@angular/core';
 export class BmiComponent implements OnInit {
 
   bmi!: number;
+  result!: string;
   weight!: string;
   height!: string;
+  gender!: string;
+  age!: string;
 
-  constructor() { }
+  constructor() { 
+    this.gender = "Male";
+    this.bmi = 0;
+    this.result = "-";
+   }
 
   ngOnInit(): void {
   }
 
-  // Method to calculate BMI based on user input
-  getBmi(): void {
+  // Method to get the BMI category based on the BMI value
+  getAdvancedBmiCategory(): string {
+    if (this.bmi < 16) {
+      return "Severe Thinness";
+    } else if (this.bmi >= 16 && this.bmi < 17) {
+      return "Moderate Thinness";
+    } else if (this.bmi >= 17 && this.bmi < 18.5) {
+      return "Mild Thinness";
+    } else if (this.bmi >= 18.5 && this.bmi < 25) {
+      return "Normal";
+    } else if (this.bmi >= 25 && this.bmi < 30) {
+      return "Overweight";
+    } else if (this.bmi >= 30 && this.bmi < 35) {
+      return "Obese Class I";
+    } else if (this.bmi >= 35 && this.bmi < 40) {
+      return "Obese Class II";
+    } else if (this.bmi >= 40) {
+      return "Obese Class III";
+    } else {
+      return "No BMI calculated";
+    }
+  }
+  
+  // Method to calculate BMI based on height, weight and gender input
+  getBmi() {
     const parsedWeight = parseInt(this.weight);
     const parsedHeight = parseInt(this.height);
     if (parsedHeight !== 0 && !isNaN(parsedWeight) && !isNaN(parsedHeight)) {
-      this.bmi = parsedWeight / ((parsedHeight * parsedHeight) / 10000);
-      alert(`Your BMI is ${this.bmi.toFixed(2)}`);
+      this.bmi = parsedWeight / (Math.pow(parsedHeight, 2)/10000);
+      this.result = `Your BMI is ${this.bmi.toFixed(2)} and you are ${this.getAdvancedBmiCategory()}`;
     } else {
-      alert("Fill in valid Data");
+      this.result = "Fill in valid data";
     }
   }
 
+  changeGender(gender: string) {
+    this.gender = gender;
+    console.log(this.gender);
+  }
 }
