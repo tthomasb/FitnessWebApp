a784fa06cd3c0597f6d40859680d688969dd7bc2
Bäuml, Thomas <baeuml@dhbw-loerrach.de>
2023-04-25 10:23:14 +0200

    update tabs (add Date)

--- Rule-the-Gym/src/app/components/nutrition/nutrition.component.ts
+++ Rule-the-Gym/src/app/components/nutrition/nutrition.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Tab {
+  label: string;
+  date: Date;
+  content: string;
+}
+
 @Component({
   selector: 'app-nutrition',
   templateUrl: './nutrition.component.html',
@@ -7,11 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NutritionComponent implements OnInit {
 
-  constructor() { }
+  dates: Date[] = [];
+  activeTabIndex = 0;
 
-  lotsOfTabs = new Array(30).fill(0).map((_, index) => `${index}`);
-
-  ngOnInit(): void {
+  constructor() { 
   }
 
+  ngOnInit(): void {
+    // Fülle die Variable "dates" mit allen Terminen im Jahr
+    const startDate = new Date(2023, 0, 1); // Startdatum: 1. Januar 2023
+    const endDate = new Date(2023, 11, 31); // Enddatum: 31. Dezember 2023
+    // const dayMs = 24 * 60 * 60 * 1000; // Anzahl der Millisekunden pro Tag
+    for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
+      this.dates.push(new Date(date.getTime()));
+    }
+
+    // Setze das aktive Tab auf das heutige Datum
+    const today = new Date();
+    const todayIndex = this.dates.findIndex(d => d.toDateString() === today.toDateString());
+    console.log(today)
+  }
 }
