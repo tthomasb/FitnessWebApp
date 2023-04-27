d69c7795480d664e0583a2f4f1b0c91052dad54a
Bäuml, Thomas <baeuml@dhbw-loerrach.de>
2023-04-27 14:47:22 +0200

    Some Tab changes

--- Rule-the-Gym/src/app/components/nutrition/nutrition.component.ts
+++ Rule-the-Gym/src/app/components/nutrition/nutrition.component.ts
@@ -3,7 +3,6 @@ import { DialogAddNutritionComponent } from 'src/app/dialogues/dialog-add-nutrit
 import { Dialog } from 'src/app/enums/dialog';
 import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
 
-
 interface Tab {
   label: string;
   date: Date;
@@ -34,24 +33,35 @@ export class NutritionComponent implements OnInit {
   protein!: number|undefined;
 
   dates: Date[] = [];
-  activeTabIndex = 0;
+  months: number[] = Array(12).fill(0).map((x, i) => ++i);
+  activeTabIndexMonth = 0;
+  activeTabIndexDay = 0;
 
-  constructor(public dialog: MatDialog) { 
+  constructor(public dialog: MatDialog) {}
+
+  getDays(month:number) {
+    var date = new Date();
+    date = new Date(date.setMonth(month));  
+    date = new Date(date.getFullYear(), date.getMonth(), 0)
+    return Array(date.getDate()).fill(0).map((x, i) => ++i)
+  }
+
+  getMonthLabel() {
+    
   }
 
   ngOnInit(): void {
-    // Fülle die Variable "dates" mit allen Terminen im Jahr
-    const startDate = new Date(2023, 0, 1); // Startdatum: 1. Januar 2023
-    const endDate = new Date(2023, 11, 31); // Enddatum: 31. Dezember 2023
+    // const startDate = new Date(2023, 0, 1); // Startdatum: 1. Januar 2023
+    // const endDate = new Date(2023, 11, 31); // Enddatum: 31. Dezember 2023
     // const dayMs = 24 * 60 * 60 * 1000; // Anzahl der Millisekunden pro Tag
-    for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
-      this.dates.push(new Date(date.getTime()));
-    }
+    // for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
+    //   this.dates.push(new Date(date.getTime())); // Erstelle ein neues Date-Objekt für jeden Tag und füge es zu "dates" hinzu
+    // }
 
     // Setze das aktive Tab auf das heutige Datum
     const today = new Date();
-    const todayIndex = this.dates.findIndex(d => d.toDateString() === today.toDateString());
-    console.log(today)
+    this.activeTabIndexDay = today.getDate() - 1;
+    this.activeTabIndexMonth = today.getMonth();
   }
 
   testAlert() {
@@ -66,5 +76,4 @@ export class NutritionComponent implements OnInit {
       data: { dialogName: Dialog.ADD }
     });
   }
-    
 }
