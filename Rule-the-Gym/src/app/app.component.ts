3e770c8aec9faa2dc0237aacebcde672d6da1ffb
Willi Zielke <willi.zielke@allegion.com>
2023-05-24 18:00:13 +0200

    Authentication works

--- Rule-the-Gym/src/app/app.component.ts
+++ Rule-the-Gym/src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Rule-the-Gym';
+  registerMode = false;
+
+  constructor(public auth: AngularFireAuth) {}
+
+  signInWithGoogle(): void {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  async signInWithEmail(email: string, password: string): Promise<void> {
+    try {
+      await this.auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      alert('Error signing in with email and password');
+    }
+  }
+
+  async registerWithEmail(email: string, password: string): Promise<void> {
+    try {
+      await this.auth.createUserWithEmailAndPassword(email, password);
+      this.registerMode = false;
+    } catch (error) {
+      alert('Error registering with email and password');
+    }
+  }
+
+  signOutClicked(): void {
+    this.auth.signOut();
+  }
 }
