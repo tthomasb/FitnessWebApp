f7cdddc7ff10e9440dbdcda42888e9e508247d94
Willi Zielke <willi.zielke@allegion.com>
2023-05-26 12:39:54 +0200

    Log In only if email accepted

--- Rule-the-Gym/src/app/app.component.ts
+++ Rule-the-Gym/src/app/app.component.ts
@@ -10,6 +10,7 @@ import firebase from 'firebase/compat/app';
 export class AppComponent {
   title = 'Rule-the-Gym';
   registerMode = false;
+  errortext: string = '';
 
   constructor(public auth: AngularFireAuth) {}
 
@@ -19,18 +20,67 @@ export class AppComponent {
 
   async signInWithEmail(email: string, password: string): Promise<void> {
     try {
-      await this.auth.signInWithEmailAndPassword(email, password);
+      const userCredential = await this.auth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+      if (userCredential.user && userCredential.user.emailVerified) {
+        // User is logged in and email is verified
+        // Proceed with your logic or redirect to the desired page
+      } else {
+        alert('Please verify your email address before logging in.');
+        this.auth.signOut();
+      }
     } catch (error) {
-      alert('Error signing in with email and password');
+      const email = window.localStorage.getItem('emailForRegistration');
+      // Retrieve the email from local storage
+
+      if (
+        email &&
+        (await this.auth.isSignInWithEmailLink(window.location.href))
+      ) {
+        // Verify the sign-in link
+        try {
+          await this.auth.signInWithEmailLink(email, window.location.href);
+          // Complete the registration process and log the user in
+          // ...
+        } catch (error) {
+          console.error('Error signing in with email link:', error);
+          alert('Error signing in with email and password');
+        }
+      } else {
+        console.error('Error signing in with email and password:', error);
+        alert('Error signing in with email and password');
+      }
     }
   }
 
   async registerWithEmail(email: string, password: string): Promise<void> {
     try {
-      await this.auth.createUserWithEmailAndPassword(email, password);
+      const userCredential = await this.auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      if (userCredential.user) {
+        await userCredential.user.sendEmailVerification();
+        if (!userCredential.user.emailVerified) {
+          await this.auth.signOut();
+          this.errortext =
+            'Please verify your email address before logging in.';
+          return;
+        }
+      }
       this.registerMode = false;
+      this.errortext = '';
     } catch (error) {
-      alert('Error registering with email and password');
+      if (
+        (error as firebase.FirebaseError).code === 'auth/email-already-in-use'
+      ) {
+        alert('Email already in use');
+      } else {
+        this.errortext = 'Error registering with email and password:';
+        console.error('Error registering with email and password:', error);
+      }
     }
   }
 
